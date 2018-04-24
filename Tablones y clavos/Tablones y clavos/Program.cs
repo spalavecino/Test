d9ef68ffb2468aecdec275152f803991d7fbbf83
Sebastian Palavecino <spalavecino@certant.com>
2018-04-24 17:05:25 -0300

    Agrega ej 1

---
+++ Tablones y clavos/Tablones y clavos/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tablones_y_clavos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static int Solution(int[] A, int[] B, int N, int[] C, int M)
+        {
+            int clavosNecesarios = 0;
+            bool sePuedeClavar;
+
+            for (int k = 0; k < N; k++)
+            {
+                sePuedeClavar = false;
+
+                for (int i = 0; i < M; i++)
+                {
+                    if (clavosNecesarios == i)
+                    {
+                        clavosNecesarios++;
+                    }
+
+                    if (A[k] <= C[i] && C[i] <= B[k])
+                    {
+                        sePuedeClavar = true;
+                        break;
+                    }
+                }
+
+                if (!sePuedeClavar)
+                {
+                    clavosNecesarios = -1;
+                    break;
+                }
+            }
+
+            return clavosNecesarios;
+        }
+
+    }
+}
